ea267c46ca4a46dd56f35b2df94dd8023df63dcd
Apisit Janpalee <apisit.j@anpieteam.online>
2024-03-11 19:55:00 +0700

    edit gap

--- src/app/components/WeekForecast.tsx
+++ src/app/components/WeekForecast.tsx
@@ -7,11 +7,11 @@ type Props = {
 
 const WeekForecast = ({ data }: Props) => {
   return (
-    <div className="flex flex-wrap justify-end gap-8 w-full px-6 md:px-0">
+    <div className="flex flex-wrap justify-center md:justify-end gap-8 w-full px-6 md:px-0">
       {data.forecast?.forecastday.map((day: any, index: number) => (
         <div
           key={index}
-          className=" bg-white/20 p-2 text-center rounded-lg flex flex-col items-center backdrop-blur-sm w-[10rem]"
+          className=" bg-white/20 p-2 text-center rounded-lg flex flex-col items-center backdrop-blur-sm w-[15rem] md:w-[10rem]"
         >
           <p>
             {new Date(day.date).toLocaleString("en-US", { weekday: "short", day: "numeric", month: "short" })}
